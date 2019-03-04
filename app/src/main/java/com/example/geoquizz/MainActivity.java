52e62b8f45d14568d30dd6c7448be2aca5659e52
KilianCM <chamiot.k@gmail.com>
2019-03-04 11:30:40 +0100

    Menu pour lancer le quizz, correction des layouts

--- app/src/main/java/com/example/geoquizz/MainActivity.java
+++ app/src/main/java/com/example/geoquizz/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.geoquizz;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.menu);
+        final Button button = (Button) findViewById(R.id.button_play_text);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                launchQuizz(view);
+            }
+        });
+    }
+
+    public void launchQuizz(View view) {
+        Intent intent = new Intent(this, QuizzGeolocalisation.class);
+        startActivity(intent);
     }
 }
