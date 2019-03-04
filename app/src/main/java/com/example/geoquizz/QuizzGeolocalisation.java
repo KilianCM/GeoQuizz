7bf3f0482c2317d24268d5c288c26780e53a6e7a
KilianCM <chamiot.k@gmail.com>
2019-03-04 08:33:11 +0100

    Surface convertie en KM2 au résultat de l'appel API

--- app/src/main/java/com/example/geoquizz/QuizzGeolocalisation.java
+++ app/src/main/java/com/example/geoquizz/QuizzGeolocalisation.java
@@ -119,7 +119,7 @@ public class QuizzGeolocalisation extends AppCompatActivity
                     mCity.setCode(city.getString("code"));
                     mCity.setCodeDepartment(city.getString("codeDepartement"));
                     mCity.setCodeRegion(city.getString("codeRegion"));
-                    mCity.setSurface(city.getDouble("surface"));
+                    mCity.setSurface(city.getDouble("surface")/100);
                     mCity.setPopulation(city.getString("population"));
                     mCity.setRegion(city.getJSONObject("region"));
                     mCity.setDepartment(city.getJSONObject("departement"));
