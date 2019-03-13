42bd2b09dde71d454d4340f4ad7848037c7df181
KilianCM <chamiot.k@gmail.com>
2019-03-13 10:28:06 +0100

    Demande de permission sur la page parcourir pour zoomer sur la position actuelle

--- app/src/main/java/com/example/geoquizz/CitySelection.java
+++ app/src/main/java/com/example/geoquizz/CitySelection.java
@@ -1,6 +1,7 @@
 package com.example.geoquizz;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -11,15 +12,20 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -30,6 +36,8 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.net.PlacesClient;
@@ -45,6 +53,15 @@ public class CitySelection extends AppCompatActivity implements OnMapReadyCallba
     private GoogleMap mMap;
     private Place mPlace;
     AutocompleteSupportFragment placeAutoComplete;
+
+    //Identifiant de la demande de permission
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    //Si l'utilisateur à autoriser la localisation ou non
+    private Boolean mLocationPermissionGranted = false;
+    //Dernière position détéctée
+    private Location mLastKnowLocation;
+    //Permet de récuperer la position
+    private FusedLocationProviderClient mFusedLocationProviderClient;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -109,19 +126,72 @@ public class CitySelection extends AppCompatActivity implements OnMapReadyCallba
     }
 
     private void centerMapOnMyLocation() {
+        try {
+            //On Check toujours si on a le droit, sinon ca plante.
+            if(mLocationPermissionGranted){
+                //On demande à notre service de localisation de trouver la position actuelle
+                @SuppressLint("MissingPermission") Task locationResult = mFusedLocationProviderClient.getLastLocation();
+                //Puisque c'est de l'asynchrone. On ajoute un écouteur qui nous préviens quand la detection est finie.
+                //On ne sait jamais si le GPS marche, si il est lent #wiko, le temps de match des satellites.
+                // De plus la position peut-etre déduite par pléthore de systèmes : Wifi, Bluetooth, Antenne LTE, etc.)
+                locationResult.addOnCompleteListener(this, new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        //Une fois la detection effectuée, on vérifie que tout est OK
+                        if(task.isSuccessful()){
+                            //Si oui on attribue la dernière localisation à notre variable pour la garder dans un coin.
+                            mLastKnowLocation = (Location) task.getResult();
+                            LatLng coordinates = new LatLng(mLastKnowLocation.getLatitude(), mLastKnowLocation.getLongitude());
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 13));
 
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
-        LatLng coordinates = new LatLng(location.getLatitude(), location.getLongitude());
-        if (location != null) {
-            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 13));
+                            CameraPosition cameraPosition = new CameraPosition.Builder()
+                                    .target(coordinates)      // Sets the center of the map to location user
+                                    .zoom(10)                   // Sets the zoom
+                                    .build();                   // Creates a CameraPosition from the builder
+                            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                        }else {
+                            //Traitement du cas où on ne trouve pas de position.
+                            Log.d("TAG", "Current location is null. Using defaults.");
+                            Log.e("TAG", "Exception: %s", task.getException());
+                        }
+                    }
+                });
 
-            CameraPosition cameraPosition = new CameraPosition.Builder()
-                    .target(coordinates)      // Sets the center of the map to location user
-                    .zoom(10)                   // Sets the zoom
-                    .build();                   // Creates a CameraPosition from the builder
-            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        } catch (SecurityException e){
+            Log.e("TAG", e.getMessage());
+        }
+    }
+
+    //On reçoit la réponse de l'utilisateur ici
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                    this.centerMapOnMyLocation();
+                }
+                else{
+                    Toast.makeText(CitySelection.this, "Impossible de centrer sur ta position sans autorisation de localisation ! :(", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    //On demande la permission ici
+    protected void getLocationPermission(){
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
         }
     }
 
@@ -148,6 +218,8 @@ public class CitySelection extends AppCompatActivity implements OnMapReadyCallba
             return;
         }
         mMap.setMyLocationEnabled(true);
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        this.getLocationPermission();
         centerMapOnMyLocation();
     }
 
